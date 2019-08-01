99b994d78f013a61c152ff62ed917c93c8eebc45
Andy <andymendez100@gmail.com>
2019-08-01 10:56:16 -0700

    Fixed file directory and changed everything to es6

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 // Requiring necessary npm packages
-var express = require("express");
-var bodyParser = require("body-parser");
-var session = require("express-session");
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 // Requiring passport as we've configured it
-var passport = require("./config/passport");
+const passport = require("./config/passport");
 //
 // Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
-var db = require("./models");
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
 //
 // Creating express app and configuring middleware needed for authentication
-var app = express();
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -24,8 +24,8 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 //
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
