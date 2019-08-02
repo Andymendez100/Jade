23ff1932a8b2fa118360c72e34fe85de32c08087
AM98 <andymendez100@gmail.com>
2019-08-01 19:27:54 -0700

    Linked login page and did javascript for login

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = require("./models");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static("views"));
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
