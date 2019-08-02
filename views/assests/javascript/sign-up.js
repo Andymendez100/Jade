f87fe14d6473848e8d9b9685f335db8b21a5ddcd
Devin Jenkins <dvnjenks@gmail.com>
2019-08-02 10:18:49 -0700

    added document.ready on sign-up.js file

--- views/assests/javascript/sign-up.js
+++ views/assests/javascript/sign-up.js
@@ -1,4 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
+$(document).ready(function() {
+    // function that makes modal work on the sign up page...post a disclaimer 
+  document.addEventListener("DOMContentLoaded", function() {
+    var elems = document.querySelectorAll(".modal");
     var instances = M.Modal.init(elems);
   });
+});
