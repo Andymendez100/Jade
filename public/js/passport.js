1670664915926e5121012db65d2f9ac2f9589ebc
Devin Jenkins <dvnjenks@gmail.com>
2019-08-02 22:26:11 -0700

    aaded facebook keys, added key.js file,put key.js in gitignore

---
+++ public/js/passport.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+  var passport = require("passport");
+  var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+  var FacebookStrategy = require("passport-facebook").Strategy;
+  var TwitterStrategy = require("passport-twitter").Strategy;
+  const key = require("./key");
+
+  // Use the GoogleStrategy within Passport.
+  //   Strategies in Passport require a `verify` function, which accept
+  //   credentials (in this case, an accessToken, refreshToken, and Google
+  //   profile), and invoke a callback with a user object.
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: key.google.clientID,
+        clientSecret: key.google.clientSecret,
+        callbackURL: "http://www.example.com/auth/google/callback"
+      },
+      function(accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ googleId: profile.id }, function(err, user) {
+          return done(err, user);
+        });
+      }
+    )
+  );
+
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: key.facebook.clientID,
+        clientSecret: key.facebook.clientSecret,
+        callbackURL: "http://www.example.com/auth/facebook/callback"
+      },
+      function(accessToken, refreshToken, profile, done) {
+        User.findOrCreate(
+          ...function(err, user) {
+            if (err) {
+              return done(err);
+            }
+            done(null, user);
+          }
+        );
+      }
+    )
+  );
+
+  passport.use(
+    new TwitterStrategy(
+      {
+        consumerKey: key.twitter.consumerKey,
+        consumerSecret: key.twitter.consumerSecret,
+        callbackURL: "http://www.example.com/auth/twitter/callback"
+      },
+      function(token, tokenSecret, profile, done) {
+        User.findOrCreate(
+          ...function(err, user) {
+            if (err) {
+              return done(err);
+            }
+            done(null, user);
+          }
+        );
+      }
+    )
+  );
+});
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,13 +2,11 @@
 const db = require("../models");
 const passport = require("../config/passport");
 //
-module.exports = (app) => {
+module.exports = app => {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
   app.post("/api/login", passport.authenticate("local"), (req, res) => {
-
-
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
@@ -23,12 +21,14 @@ module.exports = (app) => {
     db.User.create({
       email: req.body.email,
       password: req.body.password
-    }).then(() => {
-      res.redirect(307, "/api/login");
-    }).catch((err) => {
-      console.log(err);
-      res.json(err);
-    });
+    })
+      .then(() => {
+        res.redirect(307, "/api/login");
+      })
+      .catch(err => {
+        console.log(err);
+        res.json(err);
+      });
   });
   //
   // Route for logging user out
@@ -42,8 +42,7 @@ module.exports = (app) => {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
-    }
-    else {
+    } else {
       // Otherwise send back the user's email and id
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
@@ -52,4 +51,62 @@ module.exports = (app) => {
       });
     }
   });
+  // GET /auth/google
+  //   Use passport.authenticate() as route middleware to authenticate the
+  //   request.  The first step in Google authentication will involve
+  //   redirecting the user to google.com.  After authorization, Google
+  //   will redirect the user back to this application at /auth/google/callback
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["https://www.googleapis.com/auth/plus.login"]
+    })
+  );
+
+  // GET /auth/google/callback
+  //   Use passport.authenticate() as route middleware to authenticate the
+  //   request.  If authentication fails, the user will be redirected back to the
+  //   login page.  Otherwise, the primary route function function will be called,
+  //   which, in this example, will redirect the user to the home page.
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function(req, res) {
+      res.redirect("/");
+    }
+  );
+
+  // Redirect the user to Facebook for authentication.  When complete,
+  // Facebook will redirect the user back to the application at
+  //     /auth/facebook/callback
+  app.get("/auth/facebook", passport.authenticate("facebook"));
+
+  // Facebook will redirect the user to this URL after approval.  Finish the
+  // authentication process by attempting to obtain an access token.  If
+  // access was granted, the user will be logged in.  Otherwise,
+  // authentication has failed.
+  app.get(
+    "/auth/facebook/callback",
+    passport.authenticate("facebook", {
+      successRedirect: "/",
+      failureRedirect: "/login"
+    })
+  );
+
+  // Redirect the user to Twitter for authentication.  When complete, Twitter
+  // will redirect the user back to the application at
+  //   /auth/twitter/callback
+  app.get("/auth/twitter", passport.authenticate("twitter"));
+
+  // Twitter will redirect the user to this URL after approval.  Finish the
+  // authentication process by attempting to obtain an access token.  If
+  // access was granted, the user will be logged in.  Otherwise,
+  // authentication has failed.
+  app.get(
+    "/auth/twitter/callback",
+    passport.authenticate("twitter", {
+      successRedirect: "/",
+      failureRedirect: "/login"
+    })
+  );
 };
