e2b95aa444f44553db7627d16566b5af5418f781
Devin Jenkins <dvnjenks@gmail.com>
2019-08-03 10:58:36 -0700

    made new passport js files for twitter/facebook/google

---
+++ config/passport-facebook.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  const FacebookStrategy = require("passport-facebook").Strategy;
+  const key = require("./key");
+
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: key.facebook.clientID,
+        clientSecret: key.facebook.clientSecret,
+        callbackURL: "http://www.facebook.com"
+      },
+      function(accessToken, refreshToken, profile, done) {
+        User.findOrCreate(
+          ...function(err, user) {
+            if (err) {
+              return done(err);
+            }
+            done(null, user);
+          }
+        );
+      }
+    )
+  );
+});
