f5c8870ccf6ae12645380079b452e75d58f31700
Devin Jenkins <dvnjenks@gmail.com>
2019-08-03 12:00:31 -0700

    require for facebook/google/twitter added to server.js page

--- config/passport-twitter.js
+++ config/passport-twitter.js
@@ -1,25 +1,23 @@
-$(document).ready(function() {
-  const passport = require("passport"),
-    TwitterStrategy = require("passport-twitter").Strategy;
-  const key = require("./key");
+const passport = require("passport"),
+  TwitterStrategy = require("passport-twitter").Strategy;
+const key = require("./key");
 
-  passport.use(
-    new TwitterStrategy(
-      {
-        consumerKey: key.twitter.consumerKey,
-        consumerSecret: key.twitter.consumerSecret,
-        callbackURL: "http://www.example.com/auth/twitter/callback"
-      },
-      function(token, tokenSecret, profile, done) {
-        User.findOrCreate(
-          ...function(err, user) {
-            if (err) {
-              return done(err);
-            }
-            done(null, user);
+passport.use(
+  new TwitterStrategy(
+    {
+      consumerKey: key.twitter.consumerKey,
+      consumerSecret: key.twitter.consumerSecret,
+      callbackURL: "http://www.example.com/auth/twitter/callback"
+    },
+    function(token, tokenSecret, profile, done) {
+      User.findOrCreate(
+        ...function(err, user) {
+          if (err) {
+            return done(err);
           }
-        );
-      }
-    )
-  );
-});
+          done(null, user);
+        }
+      );
+    }
+  )
+);
