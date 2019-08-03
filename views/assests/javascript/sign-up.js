7d50dc09a5f11e159f331810606fd9d7eac24ce4
Devin Jenkins <dvnjenks@gmail.com>
2019-08-03 11:20:39 -0700

    removed document.ready on passport js pages

--- views/assests/javascript/sign-up.js
+++ views/assests/javascript/sign-up.js
@@ -1,7 +1,6 @@
-$(document).ready(function() {
     // function that makes modal work on the sign up page...post a disclaimer 
   document.addEventListener("DOMContentLoaded", function() {
     var elems = document.querySelectorAll(".modal");
     var instances = M.Modal.init(elems);
   });
-});
+
