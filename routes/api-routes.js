aee990e09f6fbc05a73c0fa2fa01aef42ba78301
Andy <andymendez100@gmail.com>
2019-08-04 22:26:12 -0700

    Working on getting url for s3

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,6 @@ const db = require("../models");
 const passport = require("../config/passport");
 const AWS = require('aws-sdk');
 const Busboy = require('busboy');
-// require("../dotenv").config();
 const keys = require("../config/keys");
 //
 
@@ -11,9 +10,11 @@ const BUCKET_NAME = keys.bucket;
 const IAM_USER_KEY = keys.id;
 const IAM_USER_SECRET = keys.secret;
 
-console.log(keys);
-
-
+let s3 = new AWS.S3({
+  accessKeyId: IAM_USER_KEY,
+  secretAccessKey: IAM_USER_SECRET,
+  Bucket: BUCKET_NAME
+});
 
 function uploadToS3(file) {
   let s3bucket = new AWS.S3({
@@ -39,14 +40,6 @@ function uploadToS3(file) {
 }
 
 module.exports = app => {
-
-  // The following is an example of making file upload with additional body
-  // parameters.
-  // To make a call with PostMan
-  // Don't put any headers (content-type)
-  // Under body:
-  // check form-data
-  // Put the body with "element1": "test", "element2": image file
 
   app.post('/api/upload', function (req, res, next) {
     // This grabs the additional parameters so in this case passing in
@@ -178,4 +171,32 @@ module.exports = app => {
       failureRedirect: "/login"
     })
   );
+
+  app.get("/api/userfeed", (req, res) => {
+    var params = { Bucket: BUCKET_NAME };
+    s3.listObjects(params, function (err, data) {
+      var bucketContents = data.Contents;
+      for (var i = 0; i < bucketContents.length; i++) {
+        var urlParams = { Bucket: BUCKET_NAME, Key: bucketContents[i].Key };
+        s3.getSignedUrl('getObject', urlParams, function (err, url) {
+          console.log('the url of the image is', url);
+
+          res.send(url)
+        });
+      }
+    });
+
+    // var params = { Bucket: BUCKET_NAME };
+    // s3.listObjects(params, function (err, data) {
+    //   if (err) console.log(err, err.stack); // an error occurred
+    //   else {
+    //     console.log(data.Contents[0].Key);
+    //     //console.log(data);
+    //     let key = data.Contents[0].Key;
+    //     var params = { Bucket: 'aws-demo-ta', Key: key };
+    //     var url = s3.getSignedUrl('getObject', params);
+    //     console.log('The URL is', url);
+    //   }
+    // });
+  })
 };
