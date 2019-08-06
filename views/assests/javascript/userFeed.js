2921a2f3baffb5b49220d61991820a6c18095691
Andy <andymendez100@gmail.com>
2019-08-06 01:32:13 -0700

    Loaded images of user feed

--- views/assests/javascript/userFeed.js
+++ views/assests/javascript/userFeed.js
@@ -1,10 +1,22 @@
-// Created JS page for feed.
-
 $(document).ready(function () {
+
     const getUserFeed = () => {
         $.get("/api/userFeed").then((data) => {
-            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                var newCard = $("#welcome").append(
+                    $('<div class="card" style = "width:18rem;')
 
+
+
+                    // $('<div class="card" style="width:18rem;">'),
+                    // $(" <img class='card-img-top' src='" + data[i] + "' alt='Card image cap'>"),
+                    // $("  <div class='card-body'>"),
+                    // $("<p class='card-text'>Some quick example text to build on the card title and make up the bulk of the cards content."),
+                    // $("</p>"),
+                    // $("</div>"),
+                    // $("</div>")
+                )
+            }
         })
     }
 
