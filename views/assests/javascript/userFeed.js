af18e7b135063fa06ab0e451e238021bd4cb9c64
Andy <andymendez100@gmail.com>
2019-08-06 17:43:16 -0700

    Got cards working for user page

--- views/assests/javascript/userFeed.js
+++ views/assests/javascript/userFeed.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
     const getUserFeed = () => {
         $.get("/api/userFeed").then((data) => {
             for (let i = 0; i < data.length; i++) {
-                var newCard = $("#welcome").append(
-                    $('<div class="card" style = "width:18rem;')
+                var newCard = $("#buffer").prepend(
+                    $('<div class="card" style = "width:18rem;"> <img src="' + data[i] + '" class="card-img-top"> <div class="card=body"> <p class="card-text"> Some quick example text to build on the card title and make up the bulk of the cards content. </p> </div> </div>')
 
 
 
                     // $('<div class="card" style="width:18rem;">'),
-                    // $(" <img class='card-img-top' src='" + data[i] + "' alt='Card image cap'>"),
+                    // $(" <img class='card-img-top' src='" + data[i] + "' alt='Card image cap'/>"),
                     // $("  <div class='card-body'>"),
                     // $("<p class='card-text'>Some quick example text to build on the card title and make up the bulk of the cards content."),
                     // $("</p>"),
