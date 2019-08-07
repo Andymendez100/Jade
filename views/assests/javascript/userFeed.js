ecdb0f41db3ab13e9ae4262b5165269763c1cf19
Andy <andymendez100@gmail.com>
2019-08-07 16:29:34 -0700

    Took out public folder

--- views/assests/javascript/userFeed.js
+++ views/assests/javascript/userFeed.js
@@ -1,20 +1,12 @@
 $(document).ready(function () {
 
+    // Make a get request to the api holding our links and dynamically creating cards for the feed
     const getUserFeed = () => {
         $.get("/api/userFeed").then((data) => {
             for (let i = 0; i < data.length; i++) {
-                var newCard = $("#buffer").prepend(
+                var newCard = $("#notifs").prepend(
                     $('<div class="card" style = "width:18rem;"> <img src="' + data[i] + '" class="card-img-top"> <div class="card=body"> <p class="card-text"> Some quick example text to build on the card title and make up the bulk of the cards content. </p> </div> </div>')
 
-
-
-                    // $('<div class="card" style="width:18rem;">'),
-                    // $(" <img class='card-img-top' src='" + data[i] + "' alt='Card image cap'/>"),
-                    // $("  <div class='card-body'>"),
-                    // $("<p class='card-text'>Some quick example text to build on the card title and make up the bulk of the cards content."),
-                    // $("</p>"),
-                    // $("</div>"),
-                    // $("</div>")
                 )
             }
         })
