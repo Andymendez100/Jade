dfd3185ab87593f100e1549740454b1e928fd834
Andy <andymendez100@gmail.com>
2019-10-07 04:20:27 -0700

    Fixed user page, login, and sign up

--- views/assests/javascript/sign-up.js
+++
@@ -1,6 +0,0 @@
-// function that makes modal work on the sign up page...post a disclaimer 
-document.addEventListener("DOMContentLoaded", function () {
-    var elems = document.querySelectorAll(".modal");
-    var instances = M.Modal.init(elems);
-    
-});
--- views/assests/javascript/userFeed.js
+++ views/assests/javascript/userFeed.js
@@ -1,27 +1,17 @@
-$(document).ready(function () {
-  //Create function for event that clears all card notifications when the 'clear' button is clicked
-  $("#clearButton").on("click", function () {
-    //    alert("I've been clicked!"); 
-    $("#notifs").empty();
-    $("#welcome").empty();
-    // After notifications are cleared, text will display on the feed "No new messages! Perhaps you should submit something to liven things up?""
-  });
+$(document).ready(function() {
+  $('.modal').modal();
 
-  $("#userId").val(sessionStorage.getItem("id"));
-
-  $.get("/api/userfeed").then(function (data) {
+  $.get('/api/userfeed').then(function(data) {
     console.log(data);
     for (let i = 0; i < data.length; i++) {
-      let imgUrl = data[i].postImage;
-      let Content = data[i].postContent;
-      let price = data[i].postPrice
-
-      console.log(data[i].postImage);
-
-      $("#notifs").prepend(
-        $('<div class="card" style = "width:18rem;"> <div class="card-body"> <img src="' + data[i].postImage + '" class="img-thumbnail float-left"> <p class="lead" style="clear: both;">' + Content + " \n Asking Price \n" + price + '"</p></div></div>')
-
-      )
+      const content = data[i].postContent;
+      const price = data[i].postPrice;
+      $('.pictureDisplay').append(
+        $(
+          `<div class="card userCard">  <img src="${data[i].postImage}" class="card-img-top cardImg materialboxed"> <div class="card-body"> <p class="card-text">${content} <br >Asking Price ${price}</p></div></div>`
+        )
+      );
     }
-  })
-})
+    $('.materialboxed').materialbox();
+  });
+});
